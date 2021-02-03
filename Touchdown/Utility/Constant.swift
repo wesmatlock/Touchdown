40a119dcef2687fd3074137977d0fc5a0a5dfcb1
Wesley Matlock <wes@insoc.net>
2021-02-03 06:17:58 -0700

    Carousel
    Added in an image carousel and page tab view.

--- Touchdown/Utility/Constant.swift
+++ Touchdown/Utility/Constant.swift
@@ -2,7 +2,10 @@ import SwiftUI
 
 // DATA
 
+let players: [Player] = Bundle.main.decode("player.json")
+
 // COLOR
+
 let colorBackground: Color = Color("ColorBackground")
 let colorGray: Color = Color(UIColor.systemGray4)
 
