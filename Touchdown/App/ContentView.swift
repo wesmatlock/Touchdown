1d0e25827ce5f1081cdbc8cfc6f3508ed717dc5f
Wesley Matlock <wes@insoc.net>
2021-02-05 05:27:37 -0700

    Brand Information
    Brand Item View and Grid.

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -32,6 +32,10 @@ struct ContentView: View {
             }) //: GRID
             .padding(15)
 
+            TitleView(title: "Brands")
+
+            BrandGridView()
+            
             FooterView()
               .padding(.horizontal)
           } //: VStack
--- Touchdown/View/Home/CategoryGridView.swift
+++ Touchdown/View/Home/CategoryGridView.swift
@@ -9,7 +9,7 @@ struct CategoryGridView: View {
   var body: some View {
     ScrollView(.horizontal, showsIndicators: false, content: {
 
-      LazyHGrid(rows: gridLayout, alignment: .center, spacing: columnSpacing, pinnedViews: /*@START_MENU_TOKEN@*/[]/*@END_MENU_TOKEN@*/, content: {
+      LazyHGrid(rows: gridLayout, alignment: .center, spacing: columnSpacing, pinnedViews: [], content: {
         Section(
           header: SectionView(rotateClockwise: false),
           footer: SectionView(rotateClockwise: true)
@@ -20,9 +20,8 @@ struct CategoryGridView: View {
         }
       }) //: GRID
       .frame(height: 140)
-      .padding(.horizontal, 155)
+      .padding(.horizontal, 15)
       .padding(.vertical, 10)
-
     }) //: SCROLL
   }
 }
